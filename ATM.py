19a87f10c279e3ae32ed39e02f77a2e1b7ab56b5
AB07AB <70786567+AB07AB@users.noreply.github.com>
2021-06-05 13:00:53 -0500

    Add files via upload

---
+++ ATM.py
@@ -0,0 +1,11 @@
+class Atm:
+    def __init__(self,cardnumber,pin):
+        self.cardnumber = cardnumber
+        self.pin = pin
+
+    def check_balance(self):
+        print("Your balance is 50000")
+
+    def withdrawl(self,amount):
+        new_amount = 50000 - amount
+        print("you have withdrawn amount "+str(amount) +". Your remaining balance is "+ str(new_amount))
